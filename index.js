5fa5154c1bade689a9d60d2dfb63dfdcfd98d500
skellertor <skellertor@gmail.com>
2016-12-21 15:56:09 -0700

    added ipaddr

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var fs = require('fs'),
     yaml = require('js-yaml'),
     _ = require('underscore'),
+    ipaddr = require('ipaddr.js'),
     doc = {};
 
 module.exports.white = {
     privilege: function (req, res, next) {
-        if(_.contains(doc.whitelist, req.ip)) return next();
+        var ip = ipaddr.process(req.ip).toString();
+        if(_.contains(doc.whitelist, ip)) return next();
         return next(new Error('Access Denied'));
     }
 };
