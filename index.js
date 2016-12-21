8be0f0820eb84338720f9b962f0d1f318b14cfe2
skellertor <skellertor@gmail.com>
2016-12-21 01:34:14 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var fs = require('fs'),
+    yaml = require('js-yaml'),
+    _ = require('underscore'),
+    doc = {};
+
+module.exports.white = {
+    priviledge: function (req, res, next) {
+        if(_.contains(doc.whitelist, req.ip)) return next();
+        return next(new Error('Access Denied'));
+    }
+};
+
+module.exports.black = {
+    priviledge: function (req, res, next) {
+        if(_.contains(doc.blacklist, req.ip)) return next(new Error('Access Denied'));
+        return next();
+    }
+};
+
+module.exports.priviledge = function (req, res, next) {
+    if(_.contains(doc.blacklist, req.ip)) return next(new Error('Access Denied'));
+    if(_.contains(doc.whitelist, req.ip)) return next();
+    return next();
+};
+
+module.exports.setpriviledge = function (path) {
+    doc = yaml.safeLoad(fs.readFileSync(path, 'utf8'));
+    return;
+};
